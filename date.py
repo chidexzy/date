2115800b70d0976c3e575cee41b2cb1714e24788
chidexzy <66396748+chidexzy@users.noreply.github.com>
2021-12-24 10:19:48 +0100

    Update date.py

--- date.py
+++ date.py
@@ -3,5 +3,4 @@ from datetime import datetime
 timestamp = 1545730073
 dt_object = datetime.fromtimestamp(timestamp)
 
-print("dt_object =", dt_object)
-print("type(dt_object) =", type(dt_object))
+print dt_object)
